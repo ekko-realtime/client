f1d5f33725611164e9ce5a6008df117d2328ec2f
Drew Holbrook <drewholbrook@gmail.com>
2021-04-12 16:54:10 -0600

    instructions for client api

--- public/js/ekko-client.js
+++ public/js/ekko-client.js
@@ -12,6 +12,22 @@ class Ekko {
   }
 
   // Client emit actions will always have a channel in the payload
+  /*
+  TODO ADD METHODS TO CLIENT API
+  publish
+  subscribe
+  event listener
+  disconnect
+
+  change ekko instatiation
+    - remove channel
+
+  add listner to ekko class
+  
+  make ekko subscribe method
+  make ekko unsubscribe method
+
+  */
   publish(eventType, data) {
     const payload = {
       channel: this.channel,
@@ -26,6 +42,19 @@ class Ekko {
       callback(data);
     });
   }
+
+  /*
+  pubnub.addListener({
+    message: function(m) {
+        // handle message
+        var channelName = m.channel; // The channel to which the message was published
+        var channelGroup = m.subscription; // The channel group or wildcard subscription match (if exists)
+        var pubTT = m.timetoken; // Publish timetoken
+        var msg = m.message; // The Payload
+        var publisher = m.publisher; //The Publisher
+    },
+});
+  */
 
   disconnect() {
     this.socket.close();
